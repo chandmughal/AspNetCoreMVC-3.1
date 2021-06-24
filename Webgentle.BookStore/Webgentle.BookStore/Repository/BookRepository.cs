84d86837e6d728b4cc76304cd589ffb6389aca7f
Zeeshan Zahid <theshaan@outlook.com>
2021-06-24 12:43:55 +0500

    Model Repository Added

---
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+           return Datasource();
+        }
+        //
+        public BookModel GetBook(int ID)
+        {
+            return Datasource().Where(x => x.Id == ID).FirstOrDefault() ;
+        }
+        //
+        public List<BookModel> SearchBook(String Title,String Author)
+        {
+            return Datasource().Where(x => x.Title.Contains(Title) && x.Author.Contains(Author)).ToList();
+        }
+
+        private List<BookModel> Datasource()
+        {
+            return new List<BookModel>()
+         {
+             new BookModel(){ Id = 1 ,Title = "mvc",Author = "nitish"},
+             new BookModel(){ Id = 2 ,Title = "java",Author = "nitish"},
+             new BookModel(){ Id = 3 ,Title = "csharp",Author = "nitish"},
+             new BookModel(){ Id = 4 ,Title = "core",Author = "nitish"},
+             new BookModel(){ Id = 5 ,Title = "dotnet",Author = "nitish"}
+         };
+
+        }
+
+    }
+}
