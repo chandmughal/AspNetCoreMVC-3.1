fe041e28fe9a02e06bba1e1d966331e85256937a
Zeeshan Zahid <theshaan@outlook.com>
2021-06-24 11:51:34 +0500

    Added MVC

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    //return View();
+        //   // return "WebGentle";
+        //}
+        public string Index()
+        {
+            return "WebGentle";
+        }
+    }
+}
